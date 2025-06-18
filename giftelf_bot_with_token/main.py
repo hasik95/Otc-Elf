ef11030e3d648f05d39a7d71a57d7cf6fb2f3438
hasik95 <108847637+hasik95@users.noreply.github.com>
2025-06-18 02:07:41 +0200

    Add files via upload

---
+++ giftelf_bot_with_token/main.py
@@ -0,0 +1,15 @@
+from aiogram import Bot, Dispatcher, types, executor
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+TOKEN = os.getenv("BOT_TOKEN")
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply("Привет! Я готов к работе.")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
